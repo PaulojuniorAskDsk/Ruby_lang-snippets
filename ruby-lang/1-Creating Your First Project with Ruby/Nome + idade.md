cd547d4df98a9e593c7f1f7118200cdaa0ffdb1f
Paulo Handerson lopes dos santos junior <paulohandersonlsjunior@gmail.com>
2025-10-24 19:41:55 -0300

    Commite desafio completo

---
+++ ruby-lang/1-Creating Your First Project with Ruby/Nome + idade.md
@@ -0,0 +1,3 @@
+# Nome + idade
+
+Crie um programa para simples de leitura onde recebe a entrada com nome do usuário e saída.
---
+++ ruby-lang/1-Creating Your First Project with Ruby/script.rb
@@ -0,0 +1,7 @@
+print" Qual seu nome : "
+nome = gets.chomp
+print" e seu sobrenome : "
+sobrenome = gets.chomp
+print"digite sua idade :"
+idade= gets.chomp.to_i
+puts "ola #{nome} #{sobrenome} ! Bem vindo a empresa! Sua idade e #{idade}"
